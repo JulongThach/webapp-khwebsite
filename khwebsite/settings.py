12ee752db432434afbf1f822e710d58b0de82af3
Julong Thach <thachjulong@caisedu.com>
2025-04-08 08:55:16 +0700

    Update

--- khwebsite/settings.py
+++ khwebsite/settings.py
@@ -76,6 +76,12 @@ WSGI_APPLICATION = 'khwebsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
